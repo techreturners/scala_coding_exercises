81a027e223bd242298d68d2f3448d643a4f99644
James Heggs <james.heggs@techreturners.com>
2021-12-24 23:43:22 +0000

    Include failing tests for capitlising a word

---
+++ src/main/scala/com/techreturners/exercise001/Exercise001.scala
@@ -0,0 +1,7 @@
+package com.techreturners.exercise001
+
+class Exercise001 {
+
+  def capitalizeWord(word: String) = ""
+
+}
--- src/test/scala/com/techreturners/exercise001/Exercise001Test.scala
+++ src/test/scala/com/techreturners/exercise001/Exercise001Test.scala
@@ -4,7 +4,18 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class Exercise001Test extends AnyFlatSpec {
 
-  "An empty Set" should "have size 0" in {
-    assert(Set.empty.size == 0)
+  val exercise001 = new Exercise001
+
+  "A string with one word" should "have first letter capitalised" in {
+    assert(exercise001.capitalizeWord("hello") == "Hello")
   }
+
+  "A string with multiple words" should "only have first letter capitalised" in {
+    assert(exercise001.capitalizeWord("the quick fox") == "The quick fox")
+  }
+
+  "A string with punctuation" should "only have first letter capitalised" in {
+    assert(exercise001.capitalizeWord("oh no, bears!!!") == "Oh no, bears!!!")
+  }
+
 }
